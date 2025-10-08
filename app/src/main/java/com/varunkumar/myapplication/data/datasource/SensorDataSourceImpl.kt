a78bbbdca6791cd4b1ccb03507c820ab554c766c
varun <varunkumar2004.vk@gmail.com>
2025-10-08 11:25:39 +0530

    Sensor Data (just accelerometer yet) Source Implementation

--- app/src/main/java/com/varunkumar/myapplication/data/datasource/SensorDataSourceImpl.kt
+++ app/src/main/java/com/varunkumar/myapplication/data/datasource/SensorDataSourceImpl.kt
@@ -1,13 +1,52 @@
 package com.varunkumar.myapplication.data.datasource
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
 import com.varunkumar.myapplication.data.model.RawSensorData
+import dagger.hilt.android.qualifiers.ApplicationContext
+import jakarta.inject.Inject
+import jakarta.inject.Singleton
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.callbackFlow
 
-class AndroidSensorDataSource: SensorDataSource {
-    override fun startTracking(): Flow<RawSensorData> {
-        return flow {
+@Singleton
+class AndroidSensorDataSource @Inject constructor(
+    @ApplicationContext private val context: Context
+): SensorDataSource {
+    private val sensorManager= context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
+    override fun startTracking(): Flow<RawSensorData> = callbackFlow {
+        val callback = this
+        val listener = object : SensorEventListener {
+            override fun onSensorChanged(event: SensorEvent?) {
+                event?.let {
+                    if (it.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+                        val rawData = RawSensorData(
+                            timestamp = System.currentTimeMillis(),
+                            accX = it.values[0],
+                            accY = it.values[1],
+                            accZ = it.values[2]
+                        )
+
+                        callback.trySend(rawData)
+                    }
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor?, value: Int) {
+                Log.d("AndroidSensorDataSource; sensor: $sensor", "onAccuracyChanged: $value")
+            }
+        }
+
+        sensorManager.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        awaitClose {
+            sensorManager.unregisterListener(listener)
         }
     }
 }
