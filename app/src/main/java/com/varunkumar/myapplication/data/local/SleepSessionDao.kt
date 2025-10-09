cddbf8b5450c568ec384712d8bbc245085d8bc0a
varun <varunkumar2004.vk@gmail.com>
2025-10-09 11:08:59 +0530

    Function added to SleepSessionDao.kt for getting data.

--- app/src/main/java/com/varunkumar/myapplication/data/local/SleepSessionDao.kt
+++ app/src/main/java/com/varunkumar/myapplication/data/local/SleepSessionDao.kt
@@ -1,8 +1,14 @@
 package com.varunkumar.myapplication.data.local
 
+import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 
+@Dao
 interface SleepSessionDao {
     @Insert
     suspend fun insert(sleepData: SleepSessionEntity)
+
+    @Query("SELECT * FROM sleep_session_data")
+    suspend fun getAll(): List<SleepSessionEntity>
 }
