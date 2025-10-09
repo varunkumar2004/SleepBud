a2722f0794ec888bb877ef0652d116c32b5f28e1
varun <varunkumar2004.vk@gmail.com>
2025-10-09 11:05:15 +0530

    KSP to Kapt Implemented (switch to KSP later)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Apply the Android and Kotlin plugins using the version catalog aliases
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+//    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+}
 
-    // dagger hilt
-    id("com.google.dagger.hilt.android") version "2.57.1" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
